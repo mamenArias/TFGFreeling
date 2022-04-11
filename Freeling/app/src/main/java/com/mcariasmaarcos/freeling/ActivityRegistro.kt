6bc371b6c54af57df8f132ef836efe04473c0400
Mamen <mcariasdeharo@gmail.com>
2022-04-11 19:55:22 +0200

    Pantalla Registro
    Se ha modificado la disposición de los elementos de la pantalla registro.

--- Freeling/app/src/main/java/com/mcariasmaarcos/freeling/ActivityRegistro.kt
+++ Freeling/app/src/main/java/com/mcariasmaarcos/freeling/ActivityRegistro.kt
@@ -13,9 +13,28 @@ class ActivityRegistro : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val pronombres = arrayOf("Él", "Ella", "Elle")
-        val adapter = ArrayAdapter(this, R.layout.spinner_estilo, pronombres)
-        adapter.setDropDownViewResource(R.layout.spinner_estilo)
-        binding.spinnerPronombreRegistro.adapter = adapter
+        val pronombres = arrayOf(resources.getString(R.string.el), resources.getString(R.string.ella),
+        resources.getString(R.string.elle))
+        val adapterPron = ArrayAdapter(this, R.layout.spinner_estilo, pronombres)
+        adapterPron.setDropDownViewResource(R.layout.spinner_estilo)
+        binding.spinnerPronombreRegistro.adapter = adapterPron
+
+        val generos = arrayOf(resources.getString(R.string.hombre), resources.getString(R.string.mujer),
+            resources.getString(R.string.hombreTrans), resources.getString(R.string.mujerTrans),
+            resources.getString(R.string.androgino), resources.getString(R.string.neutro),
+            resources.getString(R.string.queer), resources.getString(R.string.noBinario),
+            resources.getString(R.string.agenero), resources.getString(R.string.bigenero),
+            resources.getString(R.string.fluido), resources.getString(R.string.poligenero), resources.getString(R.string.intergenero))
+        val adapterGen = ArrayAdapter(this, R.layout.spinner_estilo, generos)
+        adapterGen.setDropDownViewResource(R.layout.spinner_estilo)
+        binding.spinnerGeneroRegistro.adapter = adapterGen
+
+        val orientacion = arrayOf(resources.getString(R.string.hetero), resources.getString(R.string.homo),
+            resources.getString(R.string.bisexual), resources.getString(R.string.pansexual), resources.getString(R.string.asexual),
+            resources.getString(R.string.demisexual), resources.getString(R.string.lightsexual),
+            resources.getString(R.string.autosexual), resources.getString(R.string.antrosexual), resources.getString(R.string.polisexual))
+        val adapterOrient = ArrayAdapter(this, R.layout.spinner_estilo, orientacion)
+        adapterOrient.setDropDownViewResource(R.layout.spinner_estilo)
+        binding.spinnerOrientacSexRegistro.adapter = adapterOrient
     }
 }

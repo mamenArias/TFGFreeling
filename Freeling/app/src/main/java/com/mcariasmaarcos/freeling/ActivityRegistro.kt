d53b029ea21e6c770d2d56f747876358b69c843f
Mamen <mcariasdeharo@gmail.com>
2022-04-11 19:32:24 +0200

    Ajustes pantalla registro

--- Freeling/app/src/main/java/com/mcariasmaarcos/freeling/ActivityRegistro.kt
+++ Freeling/app/src/main/java/com/mcariasmaarcos/freeling/ActivityRegistro.kt
@@ -2,10 +2,20 @@ package com.mcariasmaarcos.freeling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.mcariasmaarcos.freeling.databinding.ActivityRegistroBinding
 
 class ActivityRegistro : AppCompatActivity() {
+
+    val binding by lazy { ActivityRegistroBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        setContentView(binding.root)
+
+        val pronombres = arrayOf("Él", "Ella", "Elle")
+        val adapter = ArrayAdapter(this, R.layout.spinner_estilo, pronombres)
+        adapter.setDropDownViewResource(R.layout.spinner_estilo)
+        binding.spinnerPronombreRegistro.adapter = adapter
     }
 }

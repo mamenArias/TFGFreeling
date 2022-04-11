1710e99801bf7761c7f1bb9ac1f56a6150888ef1
Mamen <mcariasdeharo@gmail.com>
2022-04-11 19:12:10 +0200

    Pantalla Inicio de sesión y registro creadas
    Se han creado ambas pantallas con un diseño simple inicialmente, que luego iremos retocando.

--- Freeling/app/src/main/java/com/mcariasmaarcos/freeling/MainActivity.kt
+++ Freeling/app/src/main/java/com/mcariasmaarcos/freeling/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.mcariasmaarcos.freeling
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mcariasmaarcos.freeling.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.botonRegistro.setOnClickListener {
+            val intent:Intent = Intent(this@MainActivity, ActivityRegistro::class.java)
+            startActivity(intent)
+        }
     }
 }

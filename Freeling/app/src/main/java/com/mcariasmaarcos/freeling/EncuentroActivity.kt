5e1adff8f384c29495e0f756a520b5d5e23533f4
Mamen <mcariasdeharo@gmail.com>
2022-04-12 19:07:17 +0200

    Creación nueva actividad Encuentro

---
+++ Freeling/app/src/main/java/com/mcariasmaarcos/freeling/EncuentroActivity.kt
@@ -0,0 +1,11 @@
+package com.mcariasmaarcos.freeling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EncuentroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_encuentro)
+    }
+}

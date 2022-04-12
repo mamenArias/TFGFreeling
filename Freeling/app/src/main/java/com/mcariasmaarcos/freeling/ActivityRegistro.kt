a2aac8a69067e39b4592b2c1a3a5b9e63bf0b754
Miguel Ángel <moneite@gmail.com>
2022-04-12 16:43:26 +0200

    Registro completo

--- Freeling/app/src/main/java/com/mcariasmaarcos/freeling/ActivityRegistro.kt
+++ Freeling/app/src/main/java/com/mcariasmaarcos/freeling/ActivityRegistro.kt
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
@@ -20,6 +21,7 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import androidx.core.view.get
 import com.mcariasmaarcos.freeling.databinding.ActivityRegistroBinding
 import java.util.jar.Manifest
 
@@ -30,75 +32,103 @@ class ActivityRegistro : AppCompatActivity() {
     private lateinit var storageRef: StorageReference
 
     private val STORAGE_PERMISSION_CODE = 123
-    private lateinit var image:Uri
+    private lateinit var image: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val pronombres = arrayOf(resources.getString(R.string.el), resources.getString(R.string.ella),
-        resources.getString(R.string.elle))
+        val pronombres = arrayOf(
+            resources.getString(R.string.el), resources.getString(R.string.ella),
+            resources.getString(R.string.elle)
+        )
         val adapterPron = ArrayAdapter(this, R.layout.spinner_estilo, pronombres)
         adapterPron.setDropDownViewResource(R.layout.spinner_estilo)
         binding.spinnerPronombreRegistro.adapter = adapterPron
 
-        val generos = arrayOf(resources.getString(R.string.hombre), resources.getString(R.string.mujer),
-            resources.getString(R.string.hombreTrans), resources.getString(R.string.mujerTrans),
-            resources.getString(R.string.androgino), resources.getString(R.string.neutro),
-            resources.getString(R.string.queer), resources.getString(R.string.noBinario),
-            resources.getString(R.string.agenero), resources.getString(R.string.bigenero),
-            resources.getString(R.string.fluido), resources.getString(R.string.poligenero), resources.getString(R.string.intergenero))
+        val generos = arrayOf(
+            resources.getString(R.string.hombre),
+            resources.getString(R.string.mujer),
+            resources.getString(R.string.hombreTrans),
+            resources.getString(R.string.mujerTrans),
+            resources.getString(R.string.androgino),
+            resources.getString(R.string.neutro),
+            resources.getString(R.string.queer),
+            resources.getString(R.string.noBinario),
+            resources.getString(R.string.agenero),
+            resources.getString(R.string.bigenero),
+            resources.getString(R.string.fluido),
+            resources.getString(R.string.poligenero),
+            resources.getString(R.string.intergenero)
+        )
         val adapterGen = ArrayAdapter(this, R.layout.spinner_estilo, generos)
         adapterGen.setDropDownViewResource(R.layout.spinner_estilo)
         binding.spinnerGeneroRegistro.adapter = adapterGen
 
-        val orientacion = arrayOf(resources.getString(R.string.hetero), resources.getString(R.string.homo),
-            resources.getString(R.string.bisexual), resources.getString(R.string.pansexual), resources.getString(R.string.asexual),
-            resources.getString(R.string.demisexual), resources.getString(R.string.lightsexual),
-            resources.getString(R.string.autosexual), resources.getString(R.string.antrosexual), resources.getString(R.string.polisexual))
+        val orientacion = arrayOf(
+            resources.getString(R.string.hetero),
+            resources.getString(R.string.homo),
+            resources.getString(R.string.bisexual),
+            resources.getString(R.string.pansexual),
+            resources.getString(R.string.asexual),
+            resources.getString(R.string.demisexual),
+            resources.getString(R.string.lightsexual),
+            resources.getString(R.string.autosexual),
+            resources.getString(R.string.antrosexual),
+            resources.getString(R.string.polisexual)
+        )
         val adapterOrient = ArrayAdapter(this, R.layout.spinner_estilo, orientacion)
         adapterOrient.setDropDownViewResource(R.layout.spinner_estilo)
         binding.spinnerOrientacSexRegistro.adapter = adapterOrient
 
         binding.botonRegistrarUsuario.setOnClickListener {
 
-        if(binding.campoUsuarioRegistro.text.isEmpty()||binding.campoPasswRegistro.text.isEmpty()
-            ||binding.campoNombreRegistro.text.isEmpty()){
-            //TODO completar los empty y eso para hacer todas las comprobaciones necesarias
-        }else{
-            val auth=FirebaseAuth.getInstance()
-            val tarea =auth.createUserWithEmailAndPassword(
-                binding.campoUsuarioRegistro.text.toString(),binding.campoPasswRegistro.text.toString()
-            )
+            if (binding.campoUsuarioRegistro.text.isNullOrEmpty() || binding.campoPasswRegistro.text.isNullOrEmpty()
+                || binding.campoNombreRegistro.text.isNullOrEmpty() ||binding.spinnerPronombreRegistro.selectedItemPosition==0
+                ||binding.spinnerGeneroRegistro.selectedItemPosition==0|| binding.spinnerOrientacSexRegistro.selectedItemPosition==0) {
+                Toast.makeText(this, R.string.camposVacios, Toast.LENGTH_SHORT).show()
+            }
+            else if(!Patterns.EMAIL_ADDRESS.matcher(binding.campoUsuarioRegistro.text).matches()) {
+                Toast.makeText(this, R.string.emailValido, Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val auth = FirebaseAuth.getInstance()
+                val tarea = auth.createUserWithEmailAndPassword(
+                    binding.campoUsuarioRegistro.text.toString(),
+                    binding.campoPasswRegistro.text.toString()
+                )
 
-            tarea.addOnCompleteListener (this, object:OnCompleteListener<AuthResult>{
-                override fun onComplete(p0: Task<AuthResult>) {
-                    if(tarea.isSuccessful){
-                        Toast.makeText(this@ActivityRegistro, "Éxito", Toast.LENGTH_SHORT).show()
+                tarea.addOnCompleteListener(this, object : OnCompleteListener<AuthResult> {
+                    override fun onComplete(p0: Task<AuthResult>) {
+                        if (tarea.isSuccessful) {
+                            Toast.makeText(this@ActivityRegistro, "Éxito", Toast.LENGTH_SHORT)
+                                .show()
+                        } else {
+                            Toast.makeText(this@ActivityRegistro, "Fallo", Toast.LENGTH_SHORT)
+                                .show()
+                        }
                     }
-                    else{
-                        Toast.makeText(this@ActivityRegistro, "Fallo", Toast.LENGTH_SHORT).show()
-                    }
-                }
 
 
-            })
+                })
 
 
+            }
+
+
+
+
+
+            binding.imagenPerfil.setOnClickListener {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    STORAGE_PERMISSION_CODE
+                )
+            }
         }
 
 
-
-
-
-        binding.imagenPerfil.setOnClickListener {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
-                STORAGE_PERMISSION_CODE)
-        }
     }
-
-
-}
 
     private val selectImageResultLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult(),
@@ -126,16 +156,12 @@ class ActivityRegistro : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == STORAGE_PERMISSION_CODE){
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 selectImage()
-            }else{
+            } else {
                 Toast.makeText(this, "Permisos denegados", Toast.LENGTH_SHORT).show()
             }
         }
     }
-
-    override fun onStart() {
-        super.onStart()
-
-    }
+}

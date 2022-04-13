0dfe42f733f74f1bf32780104e4e2e1c1b8de3bb
Mamen <mcariasdeharo@gmail.com>
2022-04-13 18:18:37 +0200

    Recycler
    Se ha creado el recycler y como prueba se obtienen los datos de algunas personas registradas en la base de datos. Funciona bien.

--- Freeling/app/src/main/java/com/mcariasmaarcos/clases/Usuario.kt
+++ Freeling/app/src/main/java/com/mcariasmaarcos/clases/Usuario.kt
@@ -4,4 +4,7 @@ data class Usuario(var email:String="",
                    var nombre:String,var pronombre:String, var genero:String,var orientacionSexual:String,
                    var edad:Int,var edadDeseadaInf:Int,var edadDeseadaSup:Int,var biografia:String
                    ,var fotoPerfil:String) {
+
+    constructor(email: String): this("", "", "", "", "", 0, 0,0,
+    "","")
 }
--- Freeling/app/src/main/java/com/mcariasmaarcos/freeling/EncuentroActivityFragment.kt
+++ Freeling/app/src/main/java/com/mcariasmaarcos/freeling/EncuentroActivityFragment.kt
@@ -1,6 +1,16 @@
 package com.mcariasmaarcos.freeling
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mcariasmaarcos.clases.Usuario
+import com.mcariasmaarcos.freeling.databinding.FragmentEncuentroActivityBinding
+import com.mcariasmaarcos.recycler.RecyclerUsuariosEncontradosAdapter
 
 /**
  * A simple [Fragment] subclass.
@@ -8,5 +18,30 @@ import androidx.fragment.app.Fragment
  * create an instance of this fragment.
  */
 class EncuentroActivityFragment : Fragment(R.layout.fragment_encuentro_activity) {
+
+    private lateinit var binding:FragmentEncuentroActivityBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        @Nullable container: ViewGroup?,
+        @Nullable savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentEncuentroActivityBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val listaUsuarios:ArrayList<String> = arrayListOf<String>()
+        listaUsuarios.add("moneite@gmail.com")
+        listaUsuarios.add("mamen@gmail.com")
+
+        val adapter:RecyclerUsuariosEncontradosAdapter = RecyclerUsuariosEncontradosAdapter(this.context,listaUsuarios)
+        binding.recyclerPersonasEncontradas.adapter = adapter
+        binding.recyclerPersonasEncontradas.layoutManager = LinearLayoutManager(this.context)
+    }
+
+
+
 
 }

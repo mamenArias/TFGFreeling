d91fb429e9a3ba6056ef590437f6dc9334f28146
Mamen <mcariasdeharo@gmail.com>
2022-05-06 18:13:24 +0200

    Prueba Chat

--- Freeling/app/src/main/java/com/mcariasmaarcos/freeling/ChatActivityFragment.kt
+++ Freeling/app/src/main/java/com/mcariasmaarcos/freeling/ChatActivityFragment.kt
@@ -1,41 +1,53 @@
 package com.mcariasmaarcos.freeling
 
 
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
-import com.google.android.gms.nearby.Nearby
-import com.google.android.gms.nearby.messages.*
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import com.mcariasmaarcos.clases.Usuario
 import com.mcariasmaarcos.freeling.databinding.FragmentChatActivityBinding
-import java.nio.charset.Charset
+import com.mcariasmaarcos.recycler.RecyclerUsuariosEncontradosAdapter
 
+private lateinit var binding: FragmentChatActivityBinding
 
-
-private lateinit var binding:FragmentChatActivityBinding
-
+private val db = FirebaseFirestore.getInstance()
 
 class ChatActivityFragment : Fragment(R.layout.fragment_chat_activity) {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        @Nullable container: ViewGroup?,
+        @Nullable savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentChatActivityBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-
-override fun onCreateView(
-    inflater: LayoutInflater,
-    @Nullable container: ViewGroup?,
-    @Nullable savedInstanceState: Bundle?
-): View? {
-    binding = FragmentChatActivityBinding.inflate(inflater, container, false)
-    return binding.root
-}
-
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var listaChats:ArrayList<String>
+        lateinit var user: Usuario
+        db.collection("Usuarios").document(Firebase.auth.currentUser!!.email.toString())
+            .get()
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    user = it.result.toObject(Usuario::class.java)!!
+                }
+            }.addOnSuccessListener {
+                if(user!=null){
+                    //Toast.makeText(this.context, "llegue", Toast.LENGTH_SHORT).show()
+                    listaChats = user!!.listaChats
+                    val adapter: RecyclerUsuariosEncontradosAdapter = RecyclerUsuariosEncontradosAdapter(this.context,listaChats)
+                    binding.recyclerChats.adapter = adapter
+                    binding.recyclerChats.layoutManager = LinearLayoutManager(this.context)
+                }
+            }
+    }
 }
